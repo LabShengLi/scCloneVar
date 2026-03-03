1159b4648b4ce103574a855c779a12f0ed0445ad
Chris Chen <xunxuanc@usc.edu>
2026-03-03 00:43:44 -0800

    Update documentation and add demo dataset

---
+++ R/data-scCloneVar_test_demo.R
@@ -0,0 +1,15 @@
+#' scCloneVar_test_demo
+#'
+#' A small Seurat object containing 20 Young and 20 Old clones
+#' derived from the in vitro cross-age dataset.
+#'
+#' The dataset includes:
+#' - RNA assay
+#' - PCA and UMAP reductions
+#' - CloneID and donor_age metadata
+#'
+#' This dataset is intended for demonstrating scCloneVar workflows.
+#'
+#' @format A Seurat object
+#' @source Internal subset of in vitro cross-age dataset
+"scCloneVar_test_demo"
